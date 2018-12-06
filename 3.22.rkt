7538c77651da7d63b2dfbf87ab9fa0cdc3d5a796
William Xuan <william.y.xuan@gmail.com>
2018-12-05 19:53:08 -0800

    Add files via upload

---
+++ 3.22.rkt
@@ -0,0 +1,54 @@
+(define (make-queue)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+
+    ; empty-queue? is written to align with the way front-ptr
+    ; and rear-ptr were given, above
+    (define (empty-queue?)
+      (if (null? (cons front-ptr rear-ptr))
+      #t
+      #f))
+
+    ; peek returns the datum at the front of the queue
+    ; peek returns #f if the queue is empty
+    (define (peek)
+      (cond ((empty-queue?) (error "Empty queue.  :-("))
+            (else (car front-ptr))))
+
+    ; insert-queue! plays out differently depending on whether the queue
+    ; is currently empty or not
+    (define (insert-queue! datum)
+      (let ((new-node (cons datum '())))
+        (if (empty-queue?) 
+          (set! rear-ptr new-node)
+          (begin (set-car! front-ptr new-node)(set-car! rear-ptr new-mode)))))
+
+    ; delete-queue! has three possibilties:
+    ; * empty queue
+    ; * one element in queue
+    ; * more than one element in queue
+    (define (delete-queue!)
+      (cond ((empty-queue?) (error "Empty queue.  :-("))
+            (else 
+                    ; store the datum at the head of the queue
+                  (let ((return-value (peek)))
+                    ; update the front pointer
+                    (set! front-ptr (car rear-ptr))
+                    (set! rear-ptr (cdr rear-ptr))
+                    ; If there was only one thing in the queue, then the
+                    ; rear-ptr will need to be set to nil
+                    (if (null? front-ptr) 
+                      (set! rear-ptr null))
+                    ; Now return the element of the queue (or #f)
+                    (begin(empty-queue?) (cons front-ptr rear-ptr))))))
+
+    (define (dispatch message)
+      (cond ((eq? message 'insert-queue!) insert-queue!)
+            ((eq? message 'delete-queue!) delete-queue!)
+            ((eq? message 'peek) peek)
+            ((eq? message 'empty?) empty-queue?)))
+    dispatch))
+
+
+(define q1 (make-queue))
+
